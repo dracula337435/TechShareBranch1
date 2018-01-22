47505091f3a511a44787a8c98235a6861902b2f8
dracula337435 <dracula337435@163.com>
2018-01-22 18:58:04 +0800

    增加linux下的启动脚本，注意cp的参数加了引号

---
+++ root/src/zipRoot/bin/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+java -cp "../lib/*" org.dracula.techsharebranch1.root.MainApp org.dracula.techsharebranch1.a.A
+java -cp "../lib/*" org.dracula.techsharebranch1.root.MainApp org.dracula.techsharebranch1.b.B
+java -cp "../lib/TechShareBranch1-root-0.0.1-SNAPSHOT.jar:../lib/*a" org.dracula.techsharebranch1.root.MainApp org.dracula.techsharebranch1.b.B
